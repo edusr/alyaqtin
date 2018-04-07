73b393a9bd6a20d4473343b2681acefaf8b82da8
unknown <edus.silva.rosa@gmail.com>
2018-04-07 16:54:29 -0300

    adicionado controller e service

---
+++ src/main/java/trup/de/maluco/alyaqtin/controllers/ConfigRestController.java
@@ -0,0 +1,8 @@
+package trup.de.maluco.alyaqtin.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigRestController {
+
+}
---
+++ src/main/java/trup/de/maluco/alyaqtin/services/ConfigService.java
@@ -0,0 +1,61 @@
+package trup.de.maluco.alyaqtin.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import trup.de.maluco.alyaqtin.domain.ConfigFile;
+import trup.de.maluco.alyaqtin.domain.EurekaClientConfig;
+import trup.de.maluco.alyaqtin.domain.EurekaConfig;
+import trup.de.maluco.alyaqtin.domain.Route;
+import trup.de.maluco.alyaqtin.domain.ZuulConfig;
+
+@Service
+public class ConfigService {
+
+	public void addZuulRoute(String nameRoute, Route route) {
+
+		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+		mapper.setSerializationInclusion(Include.NON_NULL);
+
+		try {
+
+
+			ConfigFile configFile = new ConfigFile();
+			ZuulConfig zuulConfig = new ZuulConfig();
+			
+
+			EurekaConfig eurekaConfig = new EurekaConfig();
+			EurekaClientConfig eurekaClientConfig = new EurekaClientConfig();
+
+			eurekaClientConfig.setEnable(false);
+
+			eurekaConfig.setClient(eurekaClientConfig);
+
+			route.setPath("/cadastro/**");
+			route.setUrl("http://stst.peixoto.com.br:28080/cadastro/pxt-rest/cadastroRestService");
+			route.setSensitive_headers("Cookie,Set-Cookie");
+
+			configFile.setZuul(zuulConfig);
+			configFile.setEureka(eurekaConfig);
+			Map<String, Route> map = new HashMap<>();
+			map.put("cadastro", route);
+			zuulConfig.setRoutes(map);
+
+			// Write object as YAML string
+			String yaml = mapper.writeValueAsString(configFile).replaceAll("\"", "");
+
+			System.out.println(yaml);
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
